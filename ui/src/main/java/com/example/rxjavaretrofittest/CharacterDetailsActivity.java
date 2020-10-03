f79cb1e01a382114fe1c04b88af07af0444bd787
indivar verma <indivar.verma@gmail.com>
2020-10-03 13:47:14 -0400

    Removed snackbar

--- ui/src/main/java/com/example/rxjavaretrofittest/CharacterDetailsActivity.java
+++ ui/src/main/java/com/example/rxjavaretrofittest/CharacterDetailsActivity.java
@@ -39,8 +39,7 @@ public class CharacterDetailsActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
             }
         });
 
