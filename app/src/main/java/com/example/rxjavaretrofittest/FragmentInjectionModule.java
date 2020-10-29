4b4175276c9e9a43090039d82b80c2a906c03029
Vartika Srivastava <s.vartika@gmail.com>
2020-10-29 08:45:02 -0400

    ViewModel usage to create CharacterListViewModel's

--- app/src/main/java/com/example/rxjavaretrofittest/FragmentInjectionModule.java
+++ app/src/main/java/com/example/rxjavaretrofittest/FragmentInjectionModule.java
@@ -1,29 +1,28 @@
 package com.example.rxjavaretrofittest;
 
-import android.app.Activity;
-
+import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelStoreOwner;
 
 import com.example.controllers.characterslist.CharactersListModelDelegate;
 import com.example.controllers.characterslist.CharactersListViewModel;
-import com.example.controllers.characterslist.CharactersListViewModelImpl;
 import com.example.controllers.characterslist.CharactersListViewModelFactory;
+import com.example.controllers.characterslist.CharactersListViewModelImpl;
 import com.example.controllers.retrofit.CharactersListNetworkInterface;
 
 import dagger.Module;
 import dagger.Provides;
 import dagger.hilt.InstallIn;
-import dagger.hilt.android.components.ActivityComponent;
-import dagger.hilt.android.scopes.ActivityScoped;
+import dagger.hilt.android.components.FragmentComponent;
+import dagger.hilt.android.scopes.FragmentScoped;
 
 @Module
-@InstallIn(ActivityComponent.class)
-public class ActivityModule {
-    @ActivityScoped
+@InstallIn(FragmentComponent.class)
+public class FragmentInjectionModule {
+    @FragmentScoped
     @Provides
-    CharactersListViewModel getCharactersListViewModel(Activity activity, CharactersListNetworkInterface charactersListNetworkInterface) {
+    CharactersListViewModel getCharactersListViewModel(Fragment fragment, CharactersListNetworkInterface charactersListNetworkInterface) {
         ViewModelProvider.Factory factory = new CharactersListViewModelFactory(new CharactersListModelDelegate(charactersListNetworkInterface));
-        return new ViewModelProvider((ViewModelStoreOwner) activity, factory).get(CharactersListViewModelImpl.class);
+        return new ViewModelProvider((ViewModelStoreOwner) fragment, factory).get(CharactersListViewModelImpl.class);
     }
 }
