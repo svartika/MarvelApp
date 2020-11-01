0a9879e4b9abacfa0700f7d03a9e2621676dc0e3
Vartika Srivastava <s.vartika@gmail.com>
2020-11-01 15:13:07 -0500

    Injection limited to build version

--- app/src/debug/java/com/example/rxjavaretrofittest/InjectionModule2.java
+++ app/src/debug/java/com/example/rxjavaretrofittest/InjectionModule2.java
@@ -1,10 +1,15 @@
 package com.example.rxjavaretrofittest;
 
 import android.content.Context;
+import com.facebook.soloader.SoLoader;
 
 import com.example.networkcontroller.MarvelRetrofitEndpointApi;
 import com.example.networkcontroller.QueryInterceptor;
 import com.facebook.flipper.android.AndroidFlipperClient;
+import com.facebook.flipper.android.utils.FlipperUtils;
+import com.facebook.flipper.core.FlipperClient;
+import com.facebook.flipper.plugins.inspector.DescriptorMapping;
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin;
 import com.facebook.flipper.plugins.network.FlipperOkhttpInterceptor;
 import com.facebook.flipper.plugins.network.NetworkFlipperPlugin;
 import com.google.gson.Gson;
@@ -18,6 +23,7 @@ import dagger.hilt.InstallIn;
 import dagger.hilt.android.components.ApplicationComponent;
 import dagger.hilt.android.qualifiers.ApplicationContext;
 import io.reactivex.schedulers.Schedulers;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
@@ -26,20 +32,44 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 import static com.example.networkcontroller.SharedVars.BASE_URL;
 
-
 @Module
 @InstallIn(ApplicationComponent.class)
 public class InjectionModule2 {
 
-    @Provides
     @Singleton
-    public NetworkFlipperPlugin createNetworkFlipperPlugin () {
+    @Provides
+    public NetworkFlipperPlugin createNetworkFlipperPlugin() {
         return new NetworkFlipperPlugin();
     }
 
-    @Marvel
+
     @Provides
-    public Retrofit bindRetrofit(NetworkFlipperPlugin networkFlipperPlugin, @ApplicationContext Context context) {
+    @Singleton
+    public Interceptor createNetworkFlipperIntercepter (NetworkFlipperPlugin networkFlipperPlugin) {
+        return new FlipperOkhttpInterceptor(networkFlipperPlugin);
+    }
+
+
+    @Provides
+    public FlipperSetter createSetter(@ApplicationContext Context context, NetworkFlipperPlugin networkFlipperPlugin) {
+        return new FlipperSetter() {
+            @Override
+            public void setup() {
+                SoLoader.init(context, false);
+                if(BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(context)) {
+                    final FlipperClient client = AndroidFlipperClient.getInstance(context);
+                    client.addPlugin(new InspectorFlipperPlugin(context, DescriptorMapping.withDefaults()));
+
+                    client.addPlugin(networkFlipperPlugin);
+                    client.start();
+                }
+            }
+        };
+
+    }
+
+    @Provides
+    public Retrofit bindRetrofit(Interceptor networkFlipperInterceptor, @ApplicationContext Context context) {
         Gson gson = new GsonBuilder().setLenient().create();
         //RxJava2CallAdapterFactory rxAdapter = RxJava2CallAdapterFactory.create();
         RxJava2CallAdapterFactory rxAdapter = RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io());
@@ -48,12 +78,15 @@ public class InjectionModule2 {
         HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
         loggingInterceptor.level(HttpLoggingInterceptor.Level.BODY);
 
-        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+        OkHttpClient.Builder okHttpClientBuilder = new OkHttpClient.Builder()
                 .addInterceptor(new QueryInterceptor())
-                .addInterceptor(loggingInterceptor)
-                //.addNetworkInterceptor(new FlipperOkhttpInterceptor(AndroidFlipperClient.getInstance(context).getPlugin(NetworkFlipperPlugin.ID)))
-                .addNetworkInterceptor(new FlipperOkhttpInterceptor(networkFlipperPlugin))
-                .build();
+                .addInterceptor(loggingInterceptor);
+        if (networkFlipperInterceptor != null) {
+            //.addNetworkInterceptor(new FlipperOkhttpInterceptor(AndroidFlipperClient.getInstance(context).getPlugin(NetworkFlipperPlugin.ID)))
+            okHttpClientBuilder.addNetworkInterceptor(networkFlipperInterceptor);
+        }
+
+        OkHttpClient okHttpClient = okHttpClientBuilder.build();
         return new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .client(okHttpClient)
@@ -63,9 +96,14 @@ public class InjectionModule2 {
     }
 
 
+
     @Provides
-    public MarvelRetrofitEndpointApi getEndPoint(@Marvel Retrofit retrofit) {
+    public MarvelRetrofitEndpointApi getEndPoint( Retrofit retrofit) {
         return retrofit.create(MarvelRetrofitEndpointApi.class);
     }
+
+
+
+
 
 }
---
+++ app/src/main/java/com/example/rxjavaretrofittest/FlipperSetter.java
@@ -0,0 +1,5 @@
+package com.example.rxjavaretrofittest;
+
+public interface FlipperSetter {
+    void setup();
+}
